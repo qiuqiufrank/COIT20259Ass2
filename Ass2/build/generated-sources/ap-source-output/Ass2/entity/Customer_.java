fe191081e747f1c37dd633c67cef3aeddf422983
frank-laptop <qiuqiufrank@gmail.com>
2022-05-25 23:33:45 +1000

    fixed car count bug

--- Ass2/build/generated-sources/ap-source-output/Ass2/entity/Customer_.java
+++ Ass2/build/generated-sources/ap-source-output/Ass2/entity/Customer_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-05-25T20:59:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-05-25T23:24:28")
 @StaticMetamodel(Customer.class)
 public class Customer_ { 
 
